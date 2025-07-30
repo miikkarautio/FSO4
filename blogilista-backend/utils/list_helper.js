39748791a3c366b3957530d482177aeb3986376b
miikkarautio <miikka.rautio01@gmail.com>
2025-07-30 17:30:12 +0300

    4.3 & 4.4 Completed

---
+++ blogilista-backend/utils/list_helper.js
@@ -0,0 +1,18 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+    var total = 0
+
+    blogs.forEach(blog => {
+        total = total + blog.likes 
+    });
+
+    return total
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+}
