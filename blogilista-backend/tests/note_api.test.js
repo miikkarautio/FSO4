95c9d3c058c7956098bc7422547be48c6d631fb5
miikkarautio <miikka.rautio01@gmail.com>
2025-08-02 23:26:24 +0300

    4.9 Completed

--- blogilista-backend/tests/note_api.test.js
+++ blogilista-backend/tests/note_api.test.js
@@ -1,14 +1,49 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
+const Blog = require('../models/blog')
+const { before } = require('lodash')
 
 const api = supertest(app)
 
+const initialBlogs = [
+    {
+        title: "PunainenViiva",
+        author: "Ilmari Kianto",
+        url: "https://fi.wikipedia.org/wiki/Punainen_viiva_(romaani)",
+        likes: 200,
+ 
+    },
+    {
+        title: "Tulen ja jään laulu",
+        author: "George R. R. Martin",
+        url: "https://fi.wikipedia.org/wiki/Tulen_ja_j%C3%A4%C3%A4n_laulu",
+        likes: 100,
+
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
 test('Right amount of JSON blogs are returned', async () => {
     const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, 5)
+    assert.strictEqual(response.body.length, 2) 
+})
+
+test('The blogs _id is refactored correctly as "id"', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach(blog => {
+        assert.ok(blog.id) //Tarkistetaan, että "id" löydetään blogeja
+        assert.strictEqual(blog._id, undefined) //Tarkistetaan, että _id ei löydy yhtään blogia
+    })
 })
 
 after(async () => {
