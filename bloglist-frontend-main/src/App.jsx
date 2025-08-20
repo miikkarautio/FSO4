026e8aee54d18861c61e3151e3aae26a3fee319d
miikkarautio <miikka.rautio01@gmail.com>
2025-08-20 13:20:07 +0300

    loggedInUser made as a seperate function

--- bloglist-frontend-main/src/App.jsx
+++ bloglist-frontend-main/src/App.jsx
@@ -66,8 +66,16 @@ const App = () => {
       <button type='submit'>Login</button>
     </form>
   )
-  
 
+  const loggedInUser = () => (
+    <div>
+      <h2>Blogs</h2>
+      <p>{user.username} is logged in <button onClick={handleLogout}>Logout</button></p>
+      {blogsToShow.map(blog => (
+        <Blog key={blog.id} blog={blog}/>
+      ))}
+    </div>
+  )
 
   const blogForm = () => (
     <form onSubmit={addBlog}>
@@ -89,15 +97,8 @@ const App = () => {
 
   return (
     <div>
-
-       {!user && loginForm()}
-{/*        {user && blogForm()} */}
-
-      {user && <h2>blogs</h2>}
-      {user && <p>{user.username} is logged in <button onClick={handleLogout}>Logout</button></p>}
-      {blogsToShow.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {!user && loginForm()}
+      {user && loggedInUser()}
     </div>
   )
 }
